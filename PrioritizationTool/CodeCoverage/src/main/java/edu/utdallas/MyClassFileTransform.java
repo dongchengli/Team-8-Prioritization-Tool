50c4753727576f9a303833bca20956271680f8eb
dongcheng li <dxl170030@utdallas.edu>
2017-05-01 18:47:34 -0500

    Update MyclassFileTransform
    Update MyclassFileTransform

--- PrioritizationTool/CodeCoverage/src/main/java/edu/utdallas/MyClassFileTransform.java
+++ PrioritizationTool/CodeCoverage/src/main/java/edu/utdallas/MyClassFileTransform.java
@@ -10,16 +10,16 @@ import org.objectweb.asm.ClassWriter;
 import edu.utdallas.util.Helper;
 
 class MyClassFileTransform implements ClassFileTransformer {
-
-	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
-			ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-		if(className.contains("org/ahocorasick") || className.contains("org/apache/commons") || className.contains("org/whispersystems/bithub/tests") || className.contains("ru/yandex/qatools") || className.contains("io/gsonfire")|| className.contains("net/sourceforge/argparse4j")){
-			ClassReader cr = new ClassReader(classfileBuffer);
-			ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-			ClassTransformVisitor ca = new ClassTransformVisitor(cw);
-			cr.accept(ca, 0);
-			return cw.toByteArray();
-		}
-		return classfileBuffer;
-	}
+    
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if(className.contains("org/ahocorasick") || className.contains("org/apache/commons") || className.contains("org/whispersystems/bithub/tests") || className.contains("ru/yandex/qatools") || className.contains("io/gsonfire")|| className.contains("net/sourceforge/argparse4j")|| className.contains("org/ardverk/collection")|| className.contains("org/reflections")|| className.contains("org/redline_rpm")|| className.contains("org/sansorm")){
+            ClassReader cr = new ClassReader(classfileBuffer);
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            ClassTransformVisitor ca = new ClassTransformVisitor(cw);
+            cr.accept(ca, 0);
+            return cw.toByteArray();
+        }
+        return classfileBuffer;
+    }
 }
